1b3c59a67f379c219e87ccb13c10389d02f8d0f0
rainyel97 <85095908+rainyel97@users.noreply.github.com>
2022-09-26 05:45:46 +0900

    카카오 api 오류 미수정...

--- screens/SearchDrug.js
+++ screens/SearchDrug.js
@@ -1,68 +1,75 @@
 import { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Alert,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { View, Text, StyleSheet, Alert, FlatList } from "react-native";
 import axios from "axios";
 import * as Location from "expo-location";
 
 function SearchDrug() {
+  let i = 0;
   const [x, setX] = useState();
   const [y, setY] = useState();
   const [data, setData] = useState([]);
-  //권한요청
+
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        Alert.alert(
-          "현재위치를 찾을 수 없습니다.",
-          "위치 권한 요청이 거부되었습니다."
-        );
-        return;
-      }
-
-      const { coords } = await Location.getCurrentPositionAsync();
-      setX(coords.longitude);
-      setY(coords.latitude);
-      const res = await axios
-        .get(
-          `https://dapi.kakao.com/v2/local/search/keyword.json?query=대학교"&y=${y}&x=${x}&radius=20000`,
-          {
-            headers: {
-              Authorization: "KakaoAK 5bbb60811fafca11368092edfdbc6a89",
-            },
-          }
-        )
-        .catch((error) => console.log(error));
-
-      console.log(res.data.documents);
-      setData((cur) => [...cur, res.data.documents]);
-      //console.log(data);
-    })();
+    getLocation();
+    getListKakao();
   }, []);
+  async function getLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "현재위치를 찾을 수 없습니다.",
+        "위치 권한 요청이 거부되었습니다."
+      );
+      return;
+    }
+
+    const { coords } = await Location.getCurrentPositionAsync();
+    setX(coords.longitude);
+    setY(coords.latitude);
+  }
+  async function getListKakao() {
+    try {
+      const res = await axios.get(
+        `https://dapi.kakao.com/v2/local/search/keyword.json?query=대학교"&y=${y}&x=${x}&radius=20000`,
+        {
+          headers: {
+            Authorization: "KakaoAK 5bbb60811fafca11368092edfdbc6a89",
+          },
+        }
+      );
+      setData(res.data.documents);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(data);
+  }
 
   return (
-    <ScrollView style={styles.container}>
-      {data.map((data) => (
-        <View key={data.id}>
-          <Text>{data.place_name}</Text>
-          <Text>{data.address_name}</Text>
-          <Text>{data.distance}</Text>
-          <Text>{data.place_url}</Text>
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text>{item.place_name}</Text>
+            <Text>{item.phone ? item.phone : "전화번호가 없습니다..."}</Text>
+            <Text>{item.place_url}</Text>
+            <Text>{item.distance}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  item: {
+    padding: 20,
+    fontSize: 15,
+    marginTop: 5,
   },
 });
 
